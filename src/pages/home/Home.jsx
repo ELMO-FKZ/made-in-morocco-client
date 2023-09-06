dcd2a42de30c08c708e4c40558595b1618ff3530
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-06 18:33:32 +0200

    Defening the children components of the home page component.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import Landing from "../../components/landing/Landing"
+import Services from "../../components/services/Services"
+import Collections from "../../components/collections/Collections"
+import Faqs from "../../components/faqs/Faqs"
+import Testimonials from "../../components/testimonials/Testimonials"
+import Newsletter from "../../components/newsletter/Newsletter"
+
+const Home = () => {
+
+    return (
+        <>
+        <Landing />
+        <Services />
+        <Collections />
+        <Faqs />
+        <Testimonials />
+        <Newsletter />
+        </>
+    )
+}
+
+export default Home
