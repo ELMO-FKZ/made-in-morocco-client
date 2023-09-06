2e21e3b1ed8c91f2f6c58539fb00859a461aee35
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-06 18:55:46 +0200

    Creating a Modal component.

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,24 @@
+
+import { useContext, memo } from "react"
+import { FaTimes } from "react-icons/fa"
+import { ModalContext } from "../../contexts/ModalContext"
+import "./modal.css"
+
+const Modal = memo(function Modal() {
+
+    const { showModal, showMessage, closeModal } = useContext(ModalContext)
+
+    return (
+        <div className={`backdrop ${showModal ? "" : "backdrop--hide"}`} onClick={closeModal} >
+            <div className="modal" >
+                <div className="modal__text">
+                    <h1 className="modal__heading">THANK YOU!</h1>
+                    <p>{showMessage}</p>
+                </div>
+                <button className="modal__btn" onClick={closeModal}><FaTimes /></button>
+            </div>
+        </div>
+    )
+})
+
+export default Modal
