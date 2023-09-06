2868f922bc982364048055770684546a12ffffe4
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-06 18:46:40 +0200

    Creating Collections component.

---
+++ src/components/collections/Collections.jsx
@@ -0,0 +1,45 @@
+import { useRef, memo } from "react"
+import { Link as LinkRouter } from "react-router-dom"
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi"
+import SpecialHeading from "../specialHeading/SpecialHeading"
+import collections from "../../Data/collections"
+import "./collections.css"
+
+const Collections = memo(function Collections() {
+
+    const carousel = useRef()
+
+    function handleLeftClick(e) {
+        e.preventDefault()
+        carousel.current.scrollLeft -= carousel.current.offsetWidth
+    }
+
+    function handleRightClick(e) {
+        e.preventDefault()
+        carousel.current.scrollLeft += carousel.current.offsetWidth
+    }
+    
+    return (
+        <div className="collections container" >
+            <SpecialHeading title="Collections" />
+            <button className="collections__btn" onClick={handleLeftClick} aria-label="go left" ><BiChevronLeft className="collections__chevron" /></button>
+            <button className="collections__btn" onClick={handleRightClick} aria-label="go right" ><BiChevronRight className="collections__chevron" /></button>
+            <div className="collections__carousel" ref={carousel}>
+                {
+                    collections.map((collection) => {
+                        return (
+                            <LinkRouter className="collections__link" to={`/products/${collection.name}`} key={collection.id}>
+                                <div className="collections__item">
+                                    <span className="collections__item-spn">{collection.name}</span>
+                                    <img className="collections__item-img" src={collection.image} alt="collection image" />
+                                </div>
+                            </LinkRouter>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+})
+
+export default Collections
