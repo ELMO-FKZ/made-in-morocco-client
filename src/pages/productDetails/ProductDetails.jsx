47b786b10c2842da6a0acd79318749d28c254eb0
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-06 19:42:07 +0200

    Creating product details page.

---
+++ src/pages/productDetails/ProductDetails.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { ShoppingContext } from "../../contexts/ShoppingContext"
+import useFetch from "../../hooks/useFecth"
+import SpecialHeading from "../../components/specialHeading/SpecialHeading"
+import { FaCartPlus, FaArrowRight } from "react-icons/fa"
+import "./productDetails.css"
+
+const ProductDetails = () => {
+
+    const {id} = useParams()
+    const navigate = useNavigate()
+    const {addToCart} = useContext(ShoppingContext)
+    const {products} = useFetch(`http://localhost:8000/api/products/${id}`) 
+    const newPrice = Math.round(products?.price * (1 - products?.promotion/100))
+
+    function goBack() {
+        navigate("/products")
+    }
+
+    return (
+        <div className="details container">
+            <SpecialHeading title="Details" />
+            <div className="details-info">
+                <div className="details-info__image">
+                    <img className="details-info__img" src={products?.image} alt={products?.title} />
+                </div>
+                <div className="details-info__content">
+                    <h3 className="details-info__title">{products?.title}</h3>
+                    <span className="details-info__new-price">${products?.price}</span>
+                    <span className="details-info__old-price">${newPrice}</span>
+                    <p className="details-info__description">{products?.description}</p>
+                    <button className="btn primary__btn" onClick={() => addToCart(products)}>Add to cart <FaCartPlus /></button>
+                    <button className="btn secondary__btn" onClick={() => goBack()}>Go back <FaArrowRight /></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDetails
