efdaf74f1d0b37cf29bd8f407be0b94adc781c71
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-06 18:48:56 +0200

    Creating Faqs section.

---
+++ src/Data/faqs.jsx
@@ -0,0 +1,8 @@
+const faqs = [
+    {id: 1, question: "Can you custom-make a product?", answer: "Yes, we can customize your orders. If you have a specific design that you want to have on your rug or any Moroccan handmade products, you only need to contact us, and we will design it for you."},
+    {id: 2, question: "Do I have to pay any taxes?", answer: "Customs taxes and duties are not included in our shipping costs. However, based on our experience, all the Moroccan handmade products enter the United States, Canada, and most EU countries duty-free. We recommend you double-check with your local customs office or import office concerning this to avoid any confusion."},
+    {id: 3, question: "How can I order?", answer: "Our online store is open 24/7. You can place your order any time you like. Once we receive your order, we will send you a confirmation email within a few hours. If you didn’t receive it, please make sure to check the spam folder or contact us at contact@madeinmorocco.com"},
+    {id: 4, question: "How can I order?", answer: "If you are in Morocco, you can have a tour to explore our handmade products in our stores. So you can see and high quality of their work. If you are outside Morocco, we can do a live session or send you photos and videos of the handmade products and all the high-quality materials used to make them."}
+]
+
+export default faqs
---
+++ src/components/faqs/Faqs.jsx
@@ -0,0 +1,23 @@
+import { memo } from "react"
+import SpecialHeading from "../specialHeading/SpecialHeading"
+import faqs from "../../Data/faqs"
+import FaqsItem from "./FaqsItem"
+import "./faqs.css"
+
+const Faqs = memo(function Faqs() {
+
+    return (
+        <div className="faqs container" >
+            <SpecialHeading title="Faqs" />
+            <div className="faqs__container">
+                {faqs.map((faq) => {
+                    return (
+                        <FaqsItem key={faq.id} question={faq.question} answer={faq.answer}/>
+                    )
+                })}
+            </div>
+        </div>
+    )
+})
+
+export default Faqs
