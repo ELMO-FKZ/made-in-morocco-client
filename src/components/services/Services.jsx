af593db59f45d5293690e58d588a3201a8c17e38
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-06 18:41:27 +0200

    Creating services component.

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,24 @@
+import { memo } from "react"
+import services from "../../Data/services"
+import "./services.css"
+
+const Services = memo(function Services() {
+
+    return (
+        <div className="services container" >
+            {
+                services.map((service) => {
+                    return (
+                        <div className="service" key={service.id}>
+                            {service.icon}
+                            <div className="service__title">{service.title}</div>
+                            <div>{service.subtitle}</div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+})
+
+export default Services
