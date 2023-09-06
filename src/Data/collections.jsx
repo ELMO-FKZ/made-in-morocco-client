2609992d75cb268d841b9f07d29efd01da3b4344
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-06 18:44:14 +0200

    Creating data for Collections component.

---
+++ src/Data/collections.jsx
@@ -0,0 +1,19 @@
+import img1 from "/assets/lamps.webp"
+import img2 from "/assets/pottery.webp"
+import img3 from "/assets/pillow.webp"
+import img4 from "/assets/pouf.webp"
+import img5 from "/assets/carpet.webp"
+import img6 from "/assets/cosmetic.webp"
+import img7 from "/assets/tea.webp"
+
+const collections = [
+    {id: 1, name: "Lanterns & Lamps", image: img1, path:"/shop/lanterns-lamps"},
+    {id: 2, name: "Pottery", image: img2, path:"/shop/pottery"},
+    {id: 3, name: "Pillow Covers", image: img3, path:"/shop/pillow-covers"},
+    {id: 4, name: "Leather Poufs", image: img4, path:"/shop/leather-poufs"},
+    {id: 5, name: "Carpets & Rugs", image: img5, path:"/shop/carpets-rugs"},
+    {id: 6, name: "Cosmetics", image: img6, path:"/shop/Cosmetics"},
+    {id: 7, name: "Tea sets", image: img7, path:"/shop/tea-sets"}
+]
+
+export default collections
