f167dae11f52814ef7f0e742c6b232a2c8b1f0df
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-11-01 06:13:00 +0100

    Adding a hover effect in services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,7 @@
-import { memo } from "react"
 import services from "../../data/services"
 import "./services.css"
 
-const Services = memo(function Services() {
+function Services() {
 
     return (
         <div className="services container" >
@@ -11,14 +10,18 @@ const Services = memo(function Services() {
                     return (
                         <div className="service" key={service.id}>
                             {service.icon}
-                            <div className="service__title">{service.title}</div>
-                            <div>{service.subtitle}</div>
+                            <div className="service__title">
+                                {service.title}
+                            </div>
+                            <div>
+                                {service.subtitle}
+                            </div>
                         </div>
                     )
                 })
             }
         </div>
     )
-})
+}
 
 export default Services
