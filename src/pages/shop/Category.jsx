554c518b895f0e14af4304df3ffebe6cc61fcb16
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-11-01 06:15:23 +0100

    Adding sorting products by price

--- src/pages/shop/Category.jsx
+++ src/pages/shop/Category.jsx
@@ -1,20 +1,70 @@
+import { useState } from "react"
 import { useParams } from "react-router-dom"
-import ProductItem from "../../components/productItem/ProductItemm"
+import { FaChevronDown } from "react-icons/fa"
+import { newPrice } from "../../utils/newPrice"
+import ProductItem from "../../components/productItem/ProductItem"
 import SpecialHeading from "../../components/specialHeading/SpecialHeading"
 import useFetch from "../../hooks/useFecth"
+import sortPriceItems from "../../data/sortPriceItems"
 import "./shop.css"
 
-const Category = () => {
+function Category() {
 
     const {category} = useParams()
     const {products} = useFetch(`${import.meta.env.VITE_REACT_APP_SERVER_URL}/api/products/category/${category}`) 
+    const [sortPrice, setSortPrice] = useState("Default")
+    const [extend, setExtend] = useState(false)
+
+    const handleSortChange = (e) => {
+        setSortPrice(e.target.value)
+        setExtend(false)
+    }
+
+    const sortPriceHandler = () => {
+        setExtend(prevExtend => !prevExtend)
+    }
 
     return (
         <div className="shop container">
             <SpecialHeading title={category} />
+            <div className="shop__filter">
+                <div className="shop__dropdown">
+                    <button className={`shop__dropdown-btn ${extend ? "shop_dropdown--extended" : ""}`} type="button" onClick={() => sortPriceHandler()} >
+                        <span>Sort by price</span>
+                        <FaChevronDown />
+                    </button>
+                    {
+                        extend &&
+                        <ul className="shop__dropdown-menu">
+                            {sortPriceItems.map((sortPriceItem) => (
+                                <li key={sortPriceItem.id} className="shop__dropdown-menu-item" >
+                                    <input
+                                        type="radio"
+                                        id={sortPriceItem.name}
+                                        name="price"
+                                        value={sortPriceItem.name}
+                                        checked={sortPrice === sortPriceItem.name}
+                                        onChange={(e) => handleSortChange(e)} />
+                                    <label className="shop__dropdown-menu-label" htmlFor={sortPriceItem.name}>{sortPriceItem.name}</label>
+                                </li>
+                            ))}
+                        </ul>
+                    }
+                </div>
+            </div>
             <div className="product-list" >
             {
-                products?.map(product => <ProductItem key={product._id} product={product} isSelected="All"/>)
+                products
+                ?.filter((product) => product.category == category )
+                .sort((a, b) => { 
+                    if (sortPrice === "Low to high") {
+                        return newPrice(a) - newPrice(b);
+                    } else if (sortPrice === "High to low") {
+                        return newPrice(b) - newPrice(a);
+                    } else {
+                        return 0;
+                    }})
+                .map(product => <ProductItem key={product._id} product={product} />)
             }
             </div>
         </div>
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,38 +1,105 @@
 import { useState } from "react"
-import ProductItem from "../../components/productItem/ProductItemm"
+import { FaChevronDown } from "react-icons/fa";
+import { newPrice } from "../../utils/newPrice"
+import ProductItem from "../../components/productItem/ProductItem"
 import SpecialHeading from "../../components/specialHeading/SpecialHeading"
 import subNavLinks from "../../data/subNavLinks"
 import useFetch from "../../hooks/useFecth"
+import sortPriceItems from "../../data/sortPriceItems";
 import "./shop.css"
 
-const Shop = () => {
+function Shop() {
 
     const {products} = useFetch(`${import.meta.env.VITE_REACT_APP_SERVER_URL}/api/products`) 
-    const [isSelected, setIsSelected] = useState("All")
+    const [sortCategory, setSortCategory] = useState("All")
+    const [sortPrice, setSortPrice] = useState("Default")
+    const [extend, setExtend] = useState({category: false, price: false})
 
-    const handleSelectChange = (e) => {
-        setIsSelected(e.target.value)
+    const handleSortChange = (e) => {
+        const name = e.target.name
+        if (name == "category") {
+            setSortCategory(e.target.value)
+        } else if (name == "price") {
+            setSortPrice(e.target.value)
+        }
+        setExtend({category: false, price: false})
+    }
+
+    const sortCategoryHandler = () => {
+        setExtend(prevExtend => ({category: !prevExtend.category, price: false}))
+    }
+
+    const sortPriceHandler = () => {
+        setExtend(prevExtend => ({category: false, price: !prevExtend.price}))
     }
 
     return (
         <div className="shop container">
             <SpecialHeading title="Shop" />
             <div className="shop__filter">
-                <label htmlFor="shop__filter-select">Sort by category: </label>
-                <select className="shop__filter-select" id="shop__filter-select" value={isSelected} onChange={e => handleSelectChange(e)} >
+                <div className="shop__dropdown">
+                    <button className={`shop__dropdown-btn ${extend.category ? "shop_dropdown--extended" : ""}`} type="button" onClick={() => sortCategoryHandler()} >
+                        <span>Sort by category</span>
+                        <FaChevronDown />
+                    </button>
                     {
-                        subNavLinks.map(subNavLink => {
-                            return (
-                                <option key={subNavLink.id}>{subNavLink.name}</option>
-                            )
-                        })
+                        extend.category &&
+                        <ul className="shop__dropdown-menu">
+                            {subNavLinks.map((subNavLink) => (
+                                <li key={subNavLink.id} className="shop__dropdown-menu-item">
+                                    <input
+                                        type="radio"
+                                        id={subNavLink.name}
+                                        name="category"
+                                        value={subNavLink.name}
+                                        checked={sortCategory === subNavLink.name}
+                                        onChange={(e) => handleSortChange(e)} />
+                                    <label className="shop__dropdown-menu-label" htmlFor={subNavLink.name}>{subNavLink.name}</label>
+                                </li>
+                            ))}
+                        </ul>
                     }
-                </select>
+                </div>
+                <div className="shop__dropdown">
+                    <button className={`shop__dropdown-btn ${extend.price ? "shop_dropdown--extended" : ""}`} type="button" onClick={() => sortPriceHandler()} >
+                        <span>Sort by price</span>
+                        <FaChevronDown />
+                    </button>
+                    {
+                        extend.price &&
+                        <ul className="shop__dropdown-menu">
+                            {sortPriceItems.map((sortPriceItem) => (
+                                <li key={sortPriceItem.id} className="shop__dropdown-menu-item" >
+                                    <input
+                                        type="radio"
+                                        id={sortPriceItem.name}
+                                        name="price"
+                                        value={sortPriceItem.name}
+                                        checked={sortPrice === sortPriceItem.name}
+                                        onChange={(e) => handleSortChange(e)} />
+                                    <label className="shop__dropdown-menu-label" htmlFor={sortPriceItem.name}>{sortPriceItem.name}</label>
+                                </li>
+                            ))}
+                        </ul>
+                    }
+                </div>
             </div>
             <div className="product-list" >
-            {
-                products?.map((product) => <ProductItem key={product._id} product={product} isSelected={isSelected} />)
-            }
+                {
+                    products
+                    ?.filter((product) => sortCategory === "All" || sortCategory === product.category)
+                    .sort((a, b) => { 
+                        if (sortPrice === "Low to high") {
+                            return newPrice(a) - newPrice(b);
+                        } else if (sortPrice === "High to low") {
+                            return newPrice(b) - newPrice(a);
+                        } else {
+                            return 0;
+                        }})
+                    .map((product) => (
+                        <ProductItem key={product._id} product={product} />
+                    ))
+                }
             </div>
         </div>
     )
